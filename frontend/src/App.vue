c52251f7abffa1ae93f7cfe27af651a576634427
donkw <don.khong@outlook.com>
2024-06-11 14:48:12 +0800

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- {/* <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png"/>
+  <HelloWorld/> */} -->
+  <MarkdownEditor/>
+</template>
+
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import MarkdownEditor from './components/MarkdownEditor.vue'
+</script>
+
+<style>
+#logo {
+  display: block;
+  width: 50%;
+  height: 50%;
+  margin: auto;
+  padding: 10% 0 0;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-origin: content-box;
+}
+</style>
---
+++ frontend/src/components/MarkdownEditor.vue
@@ -0,0 +1,24 @@
+<template>
+  <MdEditor v-model="state.text" :theme="state.theme"/>
+</template>
+
+<script setup>
+import { reactive  } from 'vue';
+import screenfull from 'screenfull'
+import { MdEditor, config  } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+// const text = ref('# Hello Editor');
+const state = reactive({
+  text: '',
+  theme: 'dark',
+})
+
+config({
+  editorExtensions: {
+    screenfull: {
+      instance: screenfull
+    }
+  }
+})
+</script>
